34e81754f69b88b00b87d6ebd9dcf787fe36878b
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2023-09-19 17:59:15 +0800

    Delete Praktikum Pemrograman 2/Praktikum01 directory

--- Praktikum Pemrograman 2/Praktikum01/PRAK004_2210817310018_AhmadJulianoor.java
+++
@@ -1,9 +0,0 @@
-package praktikum01;
-
-public class PRAK004_2210817310018_AhmadJulianoor {
-    public static void main(String[] args){
-        String nama = "Ahmad Julianoor";
-        int myAge = 19;
-        System.out.println("Perkenalkan, Nama Saya " + nama + " Umur Saya " + myAge);
-    }
-}
