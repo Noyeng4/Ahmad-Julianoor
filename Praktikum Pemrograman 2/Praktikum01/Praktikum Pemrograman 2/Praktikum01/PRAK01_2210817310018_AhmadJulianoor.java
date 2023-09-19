be17f39855c70ff4e30f9f421a459a2a81770bcd
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2023-09-19 17:47:43 +0800

    Add files via upload

---
+++ Praktikum Pemrograman 2/Praktikum01/Praktikum Pemrograman 2/Praktikum01/PRAK01_2210817310018_AhmadJulianoor.java
@@ -0,0 +1,9 @@
+package praktikum01;
+
+public class PRAK01_2210817310018_AhmadJulianoor {
+    public static void main(String[] args) {
+        System.out.println("Selamat Pagi Ahmad Julianoor");
+        System.out.println("Selamat Siang Ahmad Julianoor");
+        System.out.println("Selamat Malam Ahmad Julianoor");
+    }
+}
