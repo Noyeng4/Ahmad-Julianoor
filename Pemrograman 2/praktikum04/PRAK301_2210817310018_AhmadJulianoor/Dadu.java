eebac33071ea6380c3eb4306f0899482866004d9
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2023-10-31 17:15:05 +0800

    Add files via upload

---
+++ Pemrograman 2/praktikum04/PRAK301_2210817310018_AhmadJulianoor/Dadu.java
@@ -0,0 +1,14 @@
+package praktikum04.PRAK301_2210817310018_AhmadJulianoor;
+import java.util.Random;
+
+class Dadu {
+    private int nilai;
+
+    public Dadu() {
+        Random rand = new Random();
+        nilai = rand.nextInt(6) + 1;
+    }
+    public int getNilai() {
+        return nilai;
+    }
+}
