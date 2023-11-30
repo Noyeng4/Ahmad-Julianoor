a52b0d3dded89129b31ccc0b980f6289137b61a6
Noyeng4 <115410703+Noyeng4@users.noreply.github.com>
2023-11-30 15:03:12 +0800

    Delete Pemrograman 2 directory

--- Pemrograman 2/praktikum05/PRAK401_2210817310018_AhmadJulianoor/Soal1Main.java
+++
@@ -1,8 +0,0 @@
-package praktikum05.PRAK401_2210817310018_AhmadJulianoor;
-
-public class Soal1Main {
-    public static void main(String[] args) {
-        HewanPeliharaan hewanPeliharaan = HewanPeliharaan.ScanHewan();
-        hewanPeliharaan.display();
-    }
-}
